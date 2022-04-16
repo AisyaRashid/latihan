1b95a41068d3d91efdcddfdf007342ce28307470
Aisya Rashid <aisyarashid0608@gmail.com>
2022-04-16 14:36:50 +0800

    update index 2

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php echo "test";
+<?php 
+echo "test";
+echo "<table>
+    <tr>
+        <td>Nama Pengguna</td>
+        <td><input type='text' /></td>
+    </tr>
+
+    <tr>
+        <td>Kata Laluan</td>
+        <td><input type='password' /></td>
+    </tr>
+
+    <tr>
+        <td><input type='submit' /></td>
+        <td></td>
+    </tr>
+    </table>";
 ?>
